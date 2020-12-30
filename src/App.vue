fe63af09d90dc146b57a726682481e7ba1c33a53
Igor Varyvoda <cheguevaraua@gmail.com>
2020-12-30 11:48:02 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="center">
+      <img alt="Vue logo" src="./assets/logo.png"> <span style="font-size:122px;padding: 0 20px;">+</span> <img class="sirvlogo" alt="Sirv logo" src="https://sirv-cdn.sirv.com/website/img/sirv-logo.svg"/>
+    </div>
+  <HelloWorld msg="Vue3 + Sirv"/>
 </template>
 
 <script>
@@ -10,10 +12,19 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  mounted() {
+  if (typeof window.Sirv === 'undefined') {
+    let smv = document.createElement('script')
+    smv.setAttribute('src', 'https://scripts.sirv.com/sirvjs/v3/sirv.js')
+    document.head.appendChild(smv)
+    // this.loadScript('https://scripts.sirv.com/sirvjs/v3/sirv.js').then(() => {
+    //   window.Sirv.start('.off')
+    // })
   }
 }
+}
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -22,5 +33,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 200px;
+  border: 3px solid green;
+}
+.sirvlogo {
+  max-width:250px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,33 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h3> Autostart ON </h3>
+    <div class="container">
+    <div class="Sirv">
+      <div data-src="https://demo.sirv.com/example.spin"></div>
+      <div data-src="https://demo.sirv.com/image.jpg" data-type="zoom"></div>
+      <div data-src="https://demo.sirv.com/video.mp4"></div>
+    </div>
+    <h3>Autostart OFF</h3>
+    <p>Sirv Media Viewer will not initialize until you push this beautiful button below.</p>
+    <button class="glow-on-hover" v-on:click="startSirv">Start</button>
+    <div class="Sirv off" data-options="autostart:off">
+      <div data-src="https://demo.sirv.com/example.spin"></div>
+      <div data-src="https://demo.sirv.com/image.jpg" data-type="zoom"></div>
+      <div data-src="https://demo.sirv.com/video.mp4"></div>
+    </div>
+    <h3>Custom buttons</h3>
+    <div class="Sirv buttons" data-options="arrows:false">
+      <div data-src="https://demo.sirv.com/demo/apt/01.jpg" data-type="zoom"></div>
+      <div data-src="https://demo.sirv.com/demo/apt/02.jpg" data-type="zoom"></div>
+      <div data-src="https://demo.sirv.com/demo/apt/03.jpg" data-type="zoom"></div>
+      <div data-src="https://demo.sirv.com/demo/apt/04.jpg" data-type="zoom"></div>
+      <div data-src="https://demo.sirv.com/demo/apt/05.jpg" data-type="zoom"></div>
+      <div data-src="https://demo.sirv.com/demo/apt/06.jpg" data-type="zoom"></div>
+    </div>
+    <button v-on:click="previous">Previous</button>
+    <button v-on:click="next">Next</button>
+    </div>
   </div>
 </template>
 
@@ -35,6 +36,39 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+    methods: {
+    loadScript(src) {
+    return new Promise(resolve => {
+      const script = document.createElement('script')
+      script.src = src;
+      script.type = 'text/javascript'
+      script.async = true
+      script.onload = resolve
+      document.body.appendChild(script)
+    })
+    },
+    startSirv() {
+      typeof window.Sirv === 'undefined' ? this.loadScript('https://scripts.sirv.com/sirvjs/v3/sirv. js').then(() => {
+        window.Sirv.start('.off') }) : window.Sirv.start('.off')
+      // if (typeof window.Sirv === 'undefined') {
+      //   this.loadScript('https://scripts.sirv.com/sirvjs/v3/sirv.js').then(() => {
+      //     window.Sirv.start('.off')
+      //   })
+      // } else {
+      //   window.Sirv.start('.off')
+      // }
+    },
+    previous() {
+      if (window.Sirv.viewer.getInstance('.buttons').isReady()) {
+        window.Sirv.viewer.getInstance('.buttons').prev()
+      }
+    },
+    next() {
+      if (window.Sirv.viewer.getInstance('.buttons').isReady()) {
+        window.Sirv.viewer.getInstance('.buttons').next()
+      }
+    }
   }
 }
 </script>
@@ -54,5 +88,68 @@ li {
 }
 a {
   color: #42b983;
+}
+.container {
+  max-width:750px;
+  margin: 0 auto;
+}
+.glow-on-hover {
+    width: 220px;
+    height: 50px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+}
+
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+
+.glow-on-hover:active {
+    color: #000
+}
+
+.glow-on-hover:active:after {
+    background: transparent;
+}
+
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
 }
 </style>
